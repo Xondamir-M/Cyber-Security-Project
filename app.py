0e39a6faf1e4796be15a42966102540b22efa818
Khondamir Mamatov <xondamirmamatov0@gmail.com>
2026-06-30 18:14:05 +0200

    Configure SQLAlchemy and initialize database
    Added database configuration and initialization for SQLAlchemy.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key-here-change-in-production'
+app.config['SECRET_KEY'] = 'your-secret-key-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///password_manager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+    print("Database was created!")
 
 @app.route('/')
 def home():
